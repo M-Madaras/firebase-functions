0fc4a91fa5e00aa35bc076140ea291bff9773599
mason madaras <m.madaras@masons-MacBook-Air.local>
2022-07-28 15:43:24 -0400

    firebase-functions

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,19 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+app.use(cors());
+
+// setup routes...
+app.get('/test', (req, res) => {
+    res.send('Wow, it actually worked.')
+})
+app.get('/another-one', (req, res) => {
+    res.send('this one works, too!')
+})
+
+exports.api = functions.https.onRequest(app);
 
 
 exports.hellothere = functions.https.onRequest((req, res) => {
